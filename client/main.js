1a6e17c779986fc4ef683149d07b28b698237e3c
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-05-07 22:32:09 -0500

    full crud complete

--- client/main.js
+++ client/main.js
@@ -1,112 +1,137 @@
-let formContainer = document.querySelector("#form-container")
-let patternContainer = document.querySelector("#pattern-container")
-let imageInput = document.querySelector("#image")
-let nameInput = document.querySelector("#name")
-let patternInput = document.querySelector("#pattern")
-let submitBtn = document.querySelector("#submit-btn")
-let priceInput = document.querySelector("#price")
-let modal = document.querySelector("#modal")
+let formContainer = document.querySelector('#form-container')
+let patternContainer = document.querySelector('#pattern-container')
+let imageInput = document.querySelector('#image')
+let nameInput = document.querySelector('#name')
+let patternInput = document.querySelector('#pattern')
+let submitBtn = document.querySelector('#submit-btn')
+let priceInput = document.querySelector('#price')
+let modal = document.querySelector('#modal')
 
 const baseURL = `http://localhost:4500`
 
-const showPatterns = (pattern) => {
-    let editBtn = document.createElement("button")
-    let deleteBtn = document.createElement("button")
-    let patternCard = document.createElement("div")
-    patternCard.classList.add("pattern-card")
-    editBtn.textContent = "Edit"
-    editBtn.onclick = () => {
-        //updatePattern(pattern.id)
-        openModal()
-    }
-    deleteBtn.textContent = "Delete"
-    deleteBtn.onclick = () => {
-        deletePattern(pattern.id)
-    }
-    patternCard.innerHTML = `
+const showPatterns = pattern => {
+  let editBtn = document.createElement('button')
+  let deleteBtn = document.createElement('button')
+  let patternCard = document.createElement('div')
+  patternCard.classList.add('pattern-card')
+  editBtn.textContent = 'Edit'
+
+  deleteBtn.textContent = 'Delete'
+  deleteBtn.onclick = () => {
+    deletePattern(pattern.id)
+  }
+  patternCard.innerHTML = `
     <img src=${pattern.imageURL} class="pattern-img"/>
     <h3 class="pattern-name">${pattern.name}</h3>
     <h4>Price: ${pattern.price}</h4>
     <a target="_blank" href="${pattern.patternURL}">Pattern Link</a>`
-     patternCard.appendChild(editBtn)
-     patternCard.appendChild(deleteBtn)
-    patternContainer.appendChild(patternCard)
+  editBtn.onclick = () => {
+    openModal(pattern)
+  }
+  patternCard.appendChild(editBtn)
+  patternCard.appendChild(deleteBtn)
+  patternContainer.appendChild(patternCard)
 }
 
 const getPatterns = () => {
-    axios.get(`${baseURL}/patterns`)
-    .then((response) => {
-        patternContainer.textContent = ""
-        for(let i = 0; i < response.data.length; i++) {
-            showPatterns(response.data[i])
-        }
+  axios
+    .get(`${baseURL}/patterns`)
+    .then(response => {
+      patternContainer.textContent = ''
+      for (let i = 0; i < response.data.length; i++) {
+        showPatterns(response.data[i])
+      }
     })
-    .catch((error) => {
-        console.log("Error", error)
+    .catch(error => {
+      console.log('Error', error)
     })
 }
 getPatterns()
 
-const createPattern = (e) => {
-    e.preventDefault() 
-    axios.post(`${baseURL}/makePattern`, {name: nameInput.value, price: priceInput.value, imageURL: imageInput.value, patternURL: patternInput.value})
-    .then((response) => {
-        imageInput.value = "",
-        nameInput.value = "",
-        priceInput.value = "",
-        patternInput.value = ""
-        getPatterns()
-        console.log(response.data)
+const createPattern = e => {
+  e.preventDefault()
+  axios
+    .post(`${baseURL}/makePattern`, {
+      name: nameInput.value,
+      price: priceInput.value,
+      imageURL: imageInput.value,
+      patternURL: patternInput.value
     })
-    .catch((error) => {
-        console.log("Error", error)
+    .then(response => {
+      ;(imageInput.value = ''),
+      (nameInput.value = ''),
+      (priceInput.value = ''),
+      (patternInput.value = '')
+      getPatterns()
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log('Error', error)
     })
 }
 
-formContainer.addEventListener("submit", createPattern)
+formContainer.addEventListener('submit', createPattern)
 
 const deletePattern = id => {
-    axios.delete(`${baseURL}/deletePattern/${id}`)
-    .then((response) => {
-       getPatterns()
-        console.log(response.data)
+  axios
+    .delete(`${baseURL}/deletePattern/${id}`)
+    .then(response => {
+      getPatterns()
+      console.log(response.data)
     })
-    .catch((error) => {
-        console.log("Error", error)
-    })
-}
-
-const updatePattern = () => {
-    axios.put(`${baseURL}/updatePattern/${id}`)
-    .then((response) => {
-       getPatterns() 
-       console.log(response.data)
-    })
-    .catch((error) => {
-        console.log("Error", error)
+    .catch(error => {
+      console.log('Error', error)
     })
 }
 
-const openModal = () => {
-    let modalContainer = document.createElement("div")
-    let modalNameInput = document.createElement("input")
-    let modalPriceInput = document.createElement("input")
-    let modalImageInput = document.createElement("input")
-    let modalPatternInput = document.createElement("input")
-    let modalBtn = document.createElement("button")
-    modalContainer.style.display = "block"
-    modalNameInput.placeholder = "Enter name..."
-    modalPriceInput.placeholder = "Enter price..."
-    modalImageInput.placeholder = "Enter image url..."
-    modalPatternInput.placeholder = "Enter pattern url..."
-    modalBtn.textContent = "Submit"
-    modalContainer.appendChild(modalNameInput)
-    modalContainer.appendChild(modalPriceInput)
-    modalContainer.appendChild(modalImageInput)
-    modalContainer.appendChild(modalPatternInput)
-    modalContainer.appendChild(modalBtn)
-    modal.appendChild(modalContainer)
-
+const updatePattern = valueObj => {
+  axios
+    .put(`${baseURL}/updatePattern/${valueObj.id}`, { ...valueObj })
+    .then(response => {
+      getPatterns()
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log('Error', error)
+    })
 }
 
-modalBtn.addEventListener("click", updatePattern)
+const openModal = patternObj => {
+  let modalContainer = document.createElement('div')
+  let modalNameInput = document.createElement('input')
+  let modalPriceInput = document.createElement('input')
+  let modalImageInput = document.createElement('input')
+  let modalPatternInput = document.createElement('input')
+  let modalBtn = document.createElement('button')
+  modalContainer.style.display = 'block'
+  modalNameInput.placeholder = 'Enter name...'
+  modalPriceInput.placeholder = 'Enter price...'
+  modalImageInput.placeholder = 'Enter image url...'
+  modalPatternInput.placeholder = 'Enter pattern url...'
+  modalBtn.textContent = 'Submit'
+  modalNameInput.value = patternObj.name
+  modalPriceInput.value = patternObj.price
+  modalImageInput.value = patternObj.imageURL
+  modalPatternInput.value = patternObj.patternURL
+  modalContainer.appendChild(modalNameInput)
+  modalContainer.appendChild(modalPriceInput)
+  modalContainer.appendChild(modalImageInput)
+  modalContainer.appendChild(modalPatternInput)
+  modalContainer.appendChild(modalBtn)
+  modal.appendChild(modalContainer)
+  modalBtn.addEventListener('click', () => {
+    let valueObj = {
+      name: modalNameInput.value,
+      price: modalPriceInput.value,
+      imageURL: modalImageInput.value,
+      patternURL: modalPatternInput.value,
+      id: patternObj.id
+    }
+    updatePattern(valueObj)
+    modalNameInput.value = ''
+    modalPriceInput.value = ''
+    modalImageInput.value = ''
+    modalPatternInput.value = ''
+    modalContainer.style.display = 'none'
+  })
+}
--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
-const express = require("express")
-const cors = require("cors")
+const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const {getPatterns, makePattern, deletePattern, updatePattern} = require("./controller")
+const {
+  getPatterns,
+  makePattern,
+  deletePattern,
+  updatePattern
+} = require('./controller')
 
-app.get("/patterns", getPatterns)
-app.post("/makePattern", makePattern)
-app.delete("/deletePattern/:id", deletePattern)
-app.put("/updatePattern/:id", updatePattern)
+app.get('/patterns', getPatterns)
+app.post('/makePattern', makePattern)
+app.delete('/deletePattern/:id', deletePattern)
+app.put('/updatePattern/:id', updatePattern)
 
 app.listen(4500, () => {
-    console.log("Listening on port 4500")
+  console.log('Listening on port 4500')
 })
